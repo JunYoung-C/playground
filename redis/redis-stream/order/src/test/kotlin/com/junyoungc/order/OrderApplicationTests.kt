fefcd3cbd3d8eb49d41c0a31771a1d28de99b513
JunYoung-Choi <doforme524@gmail.com>
2023-09-24 18:36:37 +0900

    redis stream용 프로젝트 생성

---
+++ redis/redis-stream/order/src/test/kotlin/com/junyoungc/order/OrderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.junyoungc.order
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class OrderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
